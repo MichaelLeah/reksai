f89a1215bc6decae0ec1b9a8103d09c2d6b82c32
Michael Leah <Michael Leah>
2017-07-04 21:51:16 +0100

    Fixing package comment for doc.go file.

--- doc.go
+++ doc.go
@@ -1,4 +1,2 @@
-/**
- * Package reksai is a wrapper for the League of Legends API endpoints. (https://developer.riotgames.com/api-methods/)
- */
+// Package reksai is a wrapper for the League of Legends API endpoints. (https://developer.riotgames.com/api-methods/)
 package reksai
