70c92fce6a5a3bd5490f85d6c078e458c1af11f3
Michael Leah <Michael Leah>
2017-07-14 23:08:45 +0100

    Fixing typo in travis file scripts -> script

--- Summoner.go
+++ Summoner.go
@@ -9,3 +9,38 @@ type Summoner struct {
 	Level         int    `json:"summonerLevel"`
 	RevisionDate  int64  `json:"revisionDate"`
 }
+
+// ByName ...
+// func (s Summoner) ByName(name string, region RegionCode) (summoner *Summoner, err error) {
+// 	endpoint := fmt.Sprintf("https://%s.api.riotgames.com/lol/summoner/%s/summoners/by-name/%s", regions[region], Version, name)
+
+// 	client := &http.Client{}
+// 	req, err := http.NewRequest("GET", endpoint, nil)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("unable to create new client for request: %v", err)
+// 	}
+
+// 	req.Header.Set("X-Riot-Token", os.Getenv("API_KEY"))
+
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("unable to complete request to endpoint: %v", err)
+// 	}
+
+// 	defer resp.Body.Close()
+
+// 	if resp.StatusCode != 200 {
+// 		return nil, fmt.Errorf("request to api failed with: %v", resp.Status)
+// 	}
+
+// 	respBody, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("unable to read response body: %v", err)
+// 	}
+
+// 	if err := json.Unmarshal([]byte(respBody), &summoner); err != nil {
+// 		return nil, fmt.Errorf("unable to unmarshal response body to summoner struct: %v", err)
+// 	}
+
+// 	return summoner, nil
+// }
